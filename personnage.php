74b9b5d1a978471a8164037817de47ff4f86d16d
Dita974 <a.gentes@hotmail.com>
2022-01-03 15:31:43 +0100

    Function construct

--- personnage.php
+++ personnage.php
@@ -1,6 +1,4 @@
 <!-- Noter ici la class Personnage avec les getters et les setters -->
-
-
 <?php 
 
     class Personnage {
@@ -11,6 +9,16 @@
         private string $defense;
         private string $mana;
         public array $items;
+
+
+        public function __construct($name, $health_point, $attack, $defense, $mana, $items){
+            $this->name = $name;
+            $this->health_point = $health_point;
+            $this->attack = $attack;
+            $this->defense = $defense;
+            $this->mana = $mana;
+            $this->items = $items;
+        }
 
 
 
