1d504891785933b0d344a3479a9c025898d68129
Dita974 <a.gentes@hotmail.com>
2022-01-03 15:25:05 +0100

    initial commit

---
+++ personnage.php
@@ -0,0 +1,21 @@
+<!-- Noter ici la class Personnage avec les getters et les setters -->
+
+
+<?php 
+
+    class Personnage {
+
+        public string $name;
+        private string $health_point;
+        private string $attack;
+        private string $defense;
+        private string $mana;
+        public array $items;
+
+
+
+
+
+
+
+    }
